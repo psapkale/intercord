0a709c73517ba3d40157b7a8e50159c5488cd884
Prem <prempravinsapkale@gmail.com>
2024-01-06 18:29:58 +0530

    create test create route and fixed middleware

--- backend/src/middlewares/teacher.ts
+++ backend/src/middlewares/teacher.ts
@@ -1,15 +1,32 @@
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
+import { Teacher } from '../db';
+import { Types } from 'mongoose';
+
+export interface TeacherType extends Document {
+   _id: Types.ObjectId;
+   username: string;
+   name: string;
+   email: string;
+   password: string;
+   createdTests: [];
+}
+
+export interface CustomRequest extends Request {
+   teacherId?: Types.ObjectId;
+}
 
 async function teacherMiddleware(
-   req: Request,
+   req: CustomRequest,
    res: Response,
    next: NextFunction
 ) {
    // Todo add authentication with jwt logic here
    const token: string | undefined = req.headers.authorization;
    if (!token) {
-      return;
+      return res
+         .status(403)
+         .json({ message: 'Authorization token not provided' });
    }
    const words: string[] = token.split(' ');
    const jwtToken: string = words[1];
@@ -25,7 +42,15 @@ async function teacherMiddleware(
       return res.status(403).json({ message: 'You are not authenticated' });
    }
 
+   const teacher: TeacherType | null = await Teacher.findOne({ username });
+
+   if (!teacher) {
+      return res.status(404).json({ message: 'Teacher not found' });
+   }
+
+   req.teacherId = teacher._id;
+
    next();
 }
 
-module.exports = teacherMiddleware;
+export { teacherMiddleware };
