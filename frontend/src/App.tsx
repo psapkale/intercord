2cf3e832fe9455edafa615256f5cb3b5a03b49e5
Abhay Panchal <abpanchal951357@gmail.com>
2024-01-09 07:10:54 +0530

    Lazy loading implimented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
-import Dashboard from "./pages/Dashboard";
-import NotFound from "./pages/NotFound";
+import { lazy } from "react";
+
+const Login = lazy(() => import("./pages/Login"));
+const SignUp = lazy(() => import("./pages/SignUp"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
 function App() {
   return (
