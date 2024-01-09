41b79db49669f41511f0d9166174b9a6de9e7e19
Abhay Panchal <abpanchal951357@gmail.com>
2024-01-09 07:05:58 +0530

    Not Found Page added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import Home from "./pages/Home/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </Router>
   );
