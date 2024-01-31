d9985f101f5f2ba1129a58a8d2a61d8202c518f4
Prem <prempravinsapkale@gmail.com>
2024-01-31 07:39:30 +0530

    Test page skeleton

--- frontend/src/pages/Test.tsx
+++ frontend/src/pages/Test.tsx
@@ -1,5 +1,21 @@
+import TestCard from '@/components/TestCard';
+
 const Test = () => {
-  return <div className="w-full h-full pl-[6rem] pt-[2rem]">Test</div>;
+   // const [tests, setTests] = useState<Object[]>([]);
+
+   return (
+      <div className='w-full h-full pl-[6rem] pt-[2rem]'>
+         <h1 className='text-5xl font-bold font-mono'>Test</h1>
+         <span className='font-mono text-sm pl-2'>Plan your Tests</span>
+         <div className='w-[90%] h-[80%] mt-5 flex flex-col gap-6 p-2 overflow-y-scroll'>
+            {Array(10)
+               .fill(0)
+               .map((val, i) => (
+                  <TestCard key={i} />
+               ))}
+         </div>
+      </div>
+   );
 };
 
 export default Test;
