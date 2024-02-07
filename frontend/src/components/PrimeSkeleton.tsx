ed31ec512a9100ba5e83cf5b78560c573bfe341d
Abhay Panchal <abpanchal951357@gmail.com>
2024-02-07 11:33:59 +0530

    Responsiveness of Profile Page Skeleton DONE!

--- frontend/src/components/PrimeSkeleton.tsx
+++ frontend/src/components/PrimeSkeleton.tsx
@@ -4,8 +4,12 @@ import "primereact/resources/themes/bootstrap4-light-blue/theme.css";
 export const InputLabel = () => {
   return (
     <div className="flex flex-col">
-      <Skeleton width="8rem" className="py-4 -mb-5" />;
-      <Skeleton width="20rem" className="py-5 rounded-md" />;
+      <div className="w-[12rem] md:w-[40%] lg:w-[8rem]">
+        <Skeleton width="100%" className="py-4 -mb-5" />;
+      </div>
+      <div className="w-[90%] lg:w-[20rem]">
+        <Skeleton width="100%" className="py-5 rounded-md" />;
+      </div>
     </div>
   );
 };
