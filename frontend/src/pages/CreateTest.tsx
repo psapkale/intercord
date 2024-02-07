c6dd65ec56d995116dde537fd3d4d393aaa00ac1
Abhay Panchal <abpanchal951357@gmail.com>
2024-02-07 09:28:13 +0530

    chore: DONE with prev Button in CreateTest Page

--- frontend/src/pages/CreateTest.tsx
+++ frontend/src/pages/CreateTest.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable prefer-const */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from "react";
 import QuestionModel from "./QuestionModel";
 
@@ -8,13 +10,24 @@ export type question = {
   answerIndex: number;
 };
 
+// Subject name
+type subjectType = "C++" | "PHP" | "JAVA" | "TypeScript";
+
 const CreateTest = () => {
   const [test, setTest] = useState<question[]>([]);
   const [step, setStep] = useState(0);
+  const [totalNumberOfQuestions, setTotalNumberOfQuestions] = useState(0);
+  const [subjectName, setSubjectName] = useState<subjectType>("C++");
 
   // handling next question
   const handleNext = ({ question, options, answerIndex }: question) => {
-    setTest([...test, { question, options, answerIndex }]);
+    if (test[step] === undefined)
+      setTest([...test, { question, options, answerIndex }]);
+    else {
+      let modifiedTest = test;
+      modifiedTest[step] = { question, options, answerIndex };
+      setTest(modifiedTest);
+    }
     setStep((step) => step + 1);
   };
 
@@ -25,20 +38,87 @@ const CreateTest = () => {
 
   return (
     <div className="w-full h-full pl-[6rem] pt-[2rem]">
-      <div className="flex flex-col">
-        <h1
-          className="font-mono tracking-tight font-semibold text-[3.5rem] -mb-4"
-          id="title"
-        >
-          Create Test
-        </h1>
-        <p className="pl-1">Here you can create the test (10 mcq)</p>
+      <div className="flex flex-col gap-4 mb-[3rem]">
+        <div>
+          <h1
+            className="font-b uppercase font-bold text-[4rem] -mb-4"
+            id="title"
+          >
+            Create Test
+          </h1>
+          <p className="pl-1">Here you can create the test (10 mcq)</p>
+        </div>
+        <div>
+          <div className="flex gap-8 pl-1">
+            <div className="flex flex-col">
+              <label
+                htmlFor="totalNumberOfQuestions"
+                className="font-zyada font-bold text-2xl rounded-md"
+              >
+                Number of Questions:
+              </label>
+              <input
+                type="number"
+                id="totalNumberOfQuestions"
+                className="outline-none bg-gray-100 p-2"
+                value={totalNumberOfQuestions}
+                onChange={(e) => {
+                  setTotalNumberOfQuestions(Number(e.target.value));
+                }}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label
+                htmlFor="totalNumberOfQuestions"
+                className="font-zyada font-bold text-2xl"
+              >
+                Subject:
+              </label>
+              {/* <input
+                type="text"
+                id="totalNumberOfQuestions"
+                className="outline-none bg-gray-100 p-2 rounded-md"
+                value={subjectName}
+                onChange={(e) => {
+                  setSubjectName(e.target.value);
+                }}
+              /> */}
+              <select
+                className="outline-none bg-gray-100 p-2 rounded-md"
+                onChange={(e) => {
+                  const val = e.target.value;
+                  if (
+                    val == "C++" ||
+                    val == "JAVA" ||
+                    val == "PHP" ||
+                    val == "TypeScript"
+                  ) {
+                    setSubjectName(val);
+                  }
+                }}
+              >
+                <option value="C++">C++</option>
+                <option value="JAVA">JAVA</option>
+              </select>
+            </div>
+          </div>
+        </div>
       </div>
-      <QuestionModel
-        handleNext={handleNext}
-        handleSubmit={handleSubmit}
-        step={step}
-      />
+      {/* If Number of questions are not selected then in that case */}
+      {totalNumberOfQuestions > 0 && subjectName ? (
+        <QuestionModel
+          handleNext={handleNext}
+          handleSubmit={handleSubmit}
+          step={step}
+          totalNumberOfQuestions={totalNumberOfQuestions}
+          test={test}
+          setStep={setStep}
+        />
+      ) : (
+        <h1 className="text-5xl font-zyada">
+          Select Subject Name and Total number of questions.
+        </h1>
+      )}
     </div>
   );
 };
--- frontend/src/pages/QuestionModel.tsx
+++ frontend/src/pages/QuestionModel.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unsafe-optional-chaining */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable prefer-const */
 import { useState } from "react";
 import { question } from "./CreateTest";
@@ -6,17 +8,24 @@ const QuestionModel = ({
   handleNext,
   step,
   handleSubmit,
+  totalNumberOfQuestions,
+  setStep,
+  test,
 }: {
   handleNext: (question: question) => void;
   handleSubmit: () => void;
+  setStep: (step: any) => void;
   step: number;
+  totalNumberOfQuestions: number;
+  test: question[];
 }) => {
   const [mcq, setMcq] = useState<question>({
-    answerIndex: 1,
-    question: "",
-    options: ["", "", "", ""],
+    answerIndex: test[step] != undefined ? test[step]?.answerIndex : 1,
+    question: test[step] != undefined ? test[step]?.question : "",
+    options: test[step] != undefined ? test[step]?.options : ["", "", "", ""],
   });
 
+  //! handling going to the next
   const handleNewSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     handleNext(mcq);
@@ -26,7 +35,23 @@ const QuestionModel = ({
       options: ["", "", "", ""],
     });
 
-    if (step === 9) handleSubmit();
+    if (step === totalNumberOfQuestions - 1) handleSubmit();
+  };
+
+  //! handling going to the previous
+  const handlePrev = () => {
+    setStep((prev: any) => prev - 1);
+
+    setMcq({
+      answerIndex:
+        test[Number(step - 1)] != undefined ? test[step - 1]?.answerIndex : 1,
+      question:
+        test[Number(step - 1)] != undefined ? test[step - 1]?.question : "",
+      options:
+        test[Number(step - 1)] != undefined
+          ? [...test[Number(step - 1)]?.options]
+          : ["", "", "", ""],
+    });
   };
 
   return (
@@ -132,14 +157,27 @@ const QuestionModel = ({
             onChange={(e) => {
               setMcq({ ...mcq, answerIndex: Number(e.target.value) });
             }}
+            value={mcq.answerIndex}
           />
         </div>
-        <button
-          className="hover:bg-white hover:text-black transition-all duration-300 py-1 px-10 text-3xl rounded-md mt-6 text-white font-zyada bg-black"
-          type="submit"
-        >
-          {step < 9 ? "Next" : "Submit"}
-        </button>
+        <div className="flex gap-4">
+          <button
+            className={
+              "hover:bg-white hover:text-black transition-all duration-300 py-1 px-10 text-3xl rounded-md mt-6 text-white font-zyada bg-black " +
+              (step == 0 ? "cursor-not-allowed" : "")
+            }
+            onClick={handlePrev}
+            disabled={step == 0}
+          >
+            Prev
+          </button>
+          <button
+            type="submit"
+            className="hover:bg-white hover:text-black transition-all duration-300 py-1 px-10 text-3xl rounded-md mt-6 text-white font-zyada bg-black"
+          >
+            {step < totalNumberOfQuestions - 1 ? "Next" : "Submit"}
+          </button>
+        </div>
       </form>
     </div>
   );
