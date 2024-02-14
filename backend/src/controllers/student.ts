d74f3a827b4b156ee3c65c8ddc724ff70dbd49b4
Abhay Panchal <abpanchal951357@gmail.com>
2024-02-14 09:43:59 +0530

    feat: fetching student data based on username

--- backend/src/controllers/student.ts
+++ backend/src/controllers/student.ts
@@ -1,335 +1,355 @@
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import { Request, Response } from 'express';
-import { Score, Student, Test } from '../db';
-import { StudentType, TestType } from '../types';
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { Request, Response } from "express";
+import { Score, Student, Test } from "../db";
+import { StudentType, TestType } from "../types";
 
 interface SubjectScoreType {
-   subject: string;
-   score: number;
+  subject: string;
+  score: number;
 }
 
 // Todo all mongo logic here
 // Todo restrict creating duplicate users
 export const studentRegister = async (req: Request, res: Response) => {
-   try {
-      const { name, email, username, password } = req.body;
-      if (!name || !email || !username || !password) {
-         return res
-            .status(400)
-            .json({ message: 'Please provide name, username, and password' });
-      }
+  try {
+    const { name, email, username, password } = req.body;
+    if (!name || !email || !username || !password) {
+      return res
+        .status(400)
+        .json({ message: "Please provide name, username, and password" });
+    }
 
-      let student = await Student.findOne({ email });
+    let student = await Student.findOne({ email });
 
-      if (student) {
-         return res.status(400).json({
-            message: 'Student already exists with this email',
-         });
-      }
-
-      student = await Student.findOne({ username });
-
-      if (student) {
-         return res.status(400).json({
-            message: 'Username already taken',
-         });
-      }
-
-      const hashedPassword = await bcrypt.hash(password, 10);
-
-      student = await Student.create({
-         username,
-         name,
-         email,
-         password: hashedPassword,
-         submissions: [],
+    if (student) {
+      return res.status(400).json({
+        message: "Student already exists with this email",
       });
-      if (!student) {
-         return res.status(500).json({ message: 'Failed to create student' });
-      }
+    }
 
-      await Score.create({ candidate: student._id });
+    student = await Student.findOne({ username });
 
-      const token = jwt.sign({ username }, process.env.JWT_SECRET);
-
-      res.status(200).json({
-         message: 'Student created successfully',
-         student,
-         token,
+    if (student) {
+      return res.status(400).json({
+        message: "Username already taken",
       });
-   } catch (e) {
-      // ! Remove 'e' which might potentially show authorised details
-      console.error('Error creating student:', e);
-      res.status(500);
-   }
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    student = await Student.create({
+      username,
+      name,
+      email,
+      password: hashedPassword,
+      submissions: [],
+    });
+    if (!student) {
+      return res.status(500).json({ message: "Failed to create student" });
+    }
+
+    await Score.create({ candidate: student._id });
+
+    const token = jwt.sign({ username }, process.env.JWT_SECRET);
+
+    res.status(200).json({
+      message: "Student created successfully",
+      student,
+      token,
+    });
+  } catch (e) {
+    // ! Remove 'e' which might potentially show authorised details
+    console.error("Error creating student:", e);
+    res.status(500);
+  }
 };
 
 export const studentLogin = async (req: Request, res: Response) => {
-   try {
-      const { username, email, password } = req.body;
-      if (!email || !username || !password) {
-         return res
-            .status(400)
-            .json({ message: 'Please provide username, email and password' });
-      }
+  try {
+    const { username, email, password } = req.body;
+    if (!email || !username || !password) {
+      return res
+        .status(400)
+        .json({ message: "Please provide username, email and password" });
+    }
 
-      const student: StudentType | null = await Student.findOne({ email });
+    const student: StudentType | null = await Student.findOne({ email });
 
-      if (!student) {
-         return res.status(404).json({ message: 'Student not found' });
-      }
+    if (!student) {
+      return res.status(404).json({ message: "Student not found" });
+    }
 
-      if (student.username !== username) {
-         return res.status(411).json({ message: 'Incorrect username' });
-      }
+    if (student.username !== username) {
+      return res.status(411).json({ message: "Incorrect username" });
+    }
 
-      const isMatch = await bcrypt.compare(password, student.password);
+    const isMatch = await bcrypt.compare(password, student.password);
 
-      if (!isMatch) {
-         return res.status(411).json({ message: 'Incorrect password' });
-      }
+    if (!isMatch) {
+      return res.status(411).json({ message: "Incorrect password" });
+    }
 
-      const token = jwt.sign({ username }, process.env.JWT_SECRET);
+    const token = jwt.sign({ username }, process.env.JWT_SECRET);
 
-      res.status(200).json({
-         student,
-         message: 'Student logged in successfully',
-         token,
-      });
-   } catch (e) {
-      // ! Remove 'e' which might potentially show authorised details
-      console.error('Error logging student:', e);
-      res.status(500);
-   }
+    res.status(200).json({
+      student,
+      message: "Student logged in successfully",
+      token,
+    });
+  } catch (e) {
+    // ! Remove 'e' which might potentially show authorised details
+    console.error("Error logging student:", e);
+    res.status(500);
+  }
 };
 
 export const getMyTests = async (req: Request, res: Response) => {
-   // passing username from auth middleware
-   const { username } = res.locals;
+  // passing username from auth middleware
+  const { username } = res.locals;
 
-   const student: StudentType | null = await Student.findOne({ username });
+  const student: StudentType | null = await Student.findOne({ username });
 
-   const submissions = student?.submissions;
+  const submissions = student?.submissions;
 
-   res.status(200).json({
-      submissions,
-   });
+  res.status(200).json({
+    submissions,
+  });
 };
 
 export const getTestById = async (req: Request, res: Response) => {
-   const { testId } = req.params;
+  const { testId } = req.params;
 
-   if (!testId) {
-      return res.status(400).json({
-         message: 'Please provide test id',
-      });
-   }
+  if (!testId) {
+    return res.status(400).json({
+      message: "Please provide test id",
+    });
+  }
 
-   const test: TestType | null = await Test.findOne({ _id: testId });
+  const test: TestType | null = await Test.findOne({ _id: testId });
 
-   if (!test) {
-      return res.status(404).json({
-         message: 'Test not found',
-      });
-   }
+  if (!test) {
+    return res.status(404).json({
+      message: "Test not found",
+    });
+  }
 
-   res.status(200).json({
-      message: 'Done Successfully',
-      test,
-   });
+  res.status(200).json({
+    message: "Done Successfully",
+    test,
+  });
 };
 
 export const testSubmission = async (req: Request, res: Response) => {
-   const { testId } = req.params;
-   const { submittedAnswersIndex, marksObtained } = req.body; // from frontend
-   const { username } = res.locals;
+  const { testId } = req.params;
+  const { submittedAnswersIndex, marksObtained } = req.body; // from frontend
+  const { username } = res.locals;
 
-   if (!testId) {
-      return res.status(400).json({
-         message: 'Please provide test id',
+  if (!testId) {
+    return res.status(400).json({
+      message: "Please provide test id",
+    });
+  }
+
+  let student: StudentType | null = await Student.findOne({ username });
+
+  const test: TestType | null = await Test.findOneAndUpdate(
+    { _id: testId },
+    {
+      $push: {
+        submissions: {
+          submittedBy: student._id,
+          obtainedMarks: marksObtained,
+        },
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  student = await Student.findOneAndUpdate(
+    { username },
+    {
+      $push: {
+        submissions: {
+          test: test._id,
+          submittedAnswersIndex: submittedAnswersIndex,
+          marksObtained: marksObtained,
+          submittedAt: new Date(Date.now()),
+        },
+      },
+    },
+    { new: true }
+  );
+
+  if (!student) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+
+  if (Array.isArray(student.subjectScore)) {
+    // const allSubjectScores: SubjectScoreType[];
+
+    const subjectIndex: number = student.subjectScore.findIndex(
+      (x: SubjectScoreType) => x.subject === test.subject
+    );
+
+    if (subjectIndex === -1) {
+      student.subjectScore.push({
+        subject: test.subject,
+        score: marksObtained,
       });
-   }
+    } else {
+      student.subjectScore[subjectIndex].score += marksObtained;
+    }
 
-   let student: StudentType | null = await Student.findOne({ username });
+    await student.save();
+  } else {
+    console.error("student.subjectScore is not an array");
+  }
 
-   const test: TestType | null = await Test.findOneAndUpdate(
-      { _id: testId },
-      {
-         $push: {
-            submissions: {
-               submittedBy: student._id,
-               obtainedMarks: marksObtained,
-            },
-         },
+  // Todo resove score data
+  const score = await Score.findOneAndUpdate(
+    { candidate: student._id },
+    {
+      $inc: {
+        score: marksObtained,
       },
-      {
-         new: true,
-      }
-   );
+    },
+    { new: true }
+  );
 
-   student = await Student.findOneAndUpdate(
-      { username },
-      {
-         $push: {
-            submissions: {
-               test: test._id,
-               submittedAnswersIndex: submittedAnswersIndex,
-               marksObtained: marksObtained,
-               submittedAt: new Date(Date.now()),
-            },
-         },
-      },
-      { new: true }
-   );
-
-   if (!student) {
-      return res.status(500).json({
-         message: 'Internal server error',
-      });
-   }
-
-   if (Array.isArray(student.subjectScore)) {
-      // const allSubjectScores: SubjectScoreType[];
-
-      const subjectIndex: number = student.subjectScore.findIndex(
-         (x: SubjectScoreType) => x.subject === test.subject
-      );
-
-      if (subjectIndex === -1) {
-         student.subjectScore.push({
-            subject: test.subject,
-            score: marksObtained,
-         });
-      } else {
-         student.subjectScore[subjectIndex].score += marksObtained;
-      }
-
-      await student.save();
-   } else {
-      console.error('student.subjectScore is not an array');
-   }
-
-   // Todo resove score data
-   const score = await Score.findOneAndUpdate(
-      { candidate: student._id },
-      {
-         $inc: {
-            score: marksObtained,
-         },
-      },
-      { new: true }
-   );
-
-   res.json({
-      message: 'Test submitted successfully',
-      test,
-      student,
-      score,
-   });
+  res.json({
+    message: "Test submitted successfully",
+    test,
+    student,
+    score,
+  });
 };
 
 export const bookMarkTest = async (req: Request, res: Response) => {
-   const { testId } = req.body;
-   const { username } = res.locals;
+  const { testId } = req.body;
+  const { username } = res.locals;
 
-   const student = await Student.findOneAndUpdate({
-      username: username,
-   });
+  const student = await Student.findOneAndUpdate({
+    username: username,
+  });
 
-   if (!student) {
-      return res.status(500).json({
-         message: 'Internal server error',
-      });
-   }
+  if (!student) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 
-   let indexOfTest = student.bookmark.indexOf(testId);
-   if (indexOfTest == -1) {
-      student.bookmark.push(testId);
-   } else {
-      student.bookmark.splice(indexOfTest, 1);
-   }
+  let indexOfTest = student.bookmark.indexOf(testId);
+  if (indexOfTest == -1) {
+    student.bookmark.push(testId);
+  } else {
+    student.bookmark.splice(indexOfTest, 1);
+  }
 
-   await student.save();
+  await student.save();
 
-   if (indexOfTest == -1)
-      return res.status(200).json({
-         message: 'Test Bookmark Succesfully',
-      });
+  if (indexOfTest == -1)
+    return res.status(200).json({
+      message: "Test Bookmark Succesfully",
+    });
 
-   res.status(200).json({
-      message: 'Test remove from Bookmark Succesfully',
-   });
+  res.status(200).json({
+    message: "Test remove from Bookmark Succesfully",
+  });
 };
 
 export const getAllBookMarkTest = async (req: Request, res: Response) => {
-   const { username } = res.locals;
-   try {
-      const student = await Student.findOne({
-         username: username,
-      });
+  const { username } = res.locals;
+  try {
+    const student = await Student.findOne({
+      username: username,
+    });
 
-      const allBookmarkedTests = await Test.find({
-         _id: { $in: student.bookmark },
-      });
+    const allBookmarkedTests = await Test.find({
+      _id: { $in: student.bookmark },
+    });
 
-      res.status(200).json({
-         allBookmarkedTests,
-      });
-   } catch (error) {
-      res.status(500).json({
-         messsage: 'Some error occured',
-      });
-      console.log(error, 'Error in GetAllBookMarkedTest');
-   }
+    res.status(200).json({
+      allBookmarkedTests,
+    });
+  } catch (error) {
+    res.status(500).json({
+      messsage: "Some error occured",
+    });
+    console.log(error, "Error in GetAllBookMarkedTest");
+  }
 };
 
 export const updateStudentProfile = async (req: Request, res: Response) => {
-   let { username } = res.locals;
-   const {
-      name,
-      usernameNew,
-      password,
-      email,
-      githubUrl,
-      linkdinUrl,
-      twitterUrl,
-   } = req.body;
-   try {
-      const student = await Student.findOne({
-         username: username,
+  let { username } = res.locals;
+  const {
+    name,
+    usernameNew,
+    password,
+    email,
+    githubUrl,
+    linkdinUrl,
+    twitterUrl,
+  } = req.body;
+  try {
+    const student = await Student.findOne({
+      username: username,
+    });
+
+    if (!student) {
+      return res.status(200).json({
+        message: "Student Not Found",
       });
+    }
 
-      if (!student) {
-         return res.status(200).json({
-            message: 'Student Not Found',
-         });
-      }
+    // changing password if it is provided
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      student.password = hashedPassword;
+    }
 
-      // changing password if it is provided
-      if (password) {
-         const salt = await bcrypt.genSalt(10);
-         const hashedPassword = await bcrypt.hash(password, salt);
-         student.password = hashedPassword;
-      }
+    student.name = name || student.name;
+    student.username = usernameNew || student.username;
+    student.email = email || student.email;
+    student.githubUrl = githubUrl || student.githubUrl;
+    student.linkedinUrl = linkdinUrl || student.linkedinUrl;
+    student.twitterUrl = twitterUrl || student.twitterUrl;
 
-      student.name = name || student.name;
-      student.username = usernameNew || student.username;
-      student.email = email || student.email;
-      student.githubUrl = githubUrl || student.githubUrl;
-      student.linkedinUrl = linkdinUrl || student.linkedinUrl;
-      student.twitterUrl = twitterUrl || student.twitterUrl;
+    await student.save();
 
-      await student.save();
+    res.status(200).json({
+      message: "Student Profile Updated",
+      user: student,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Some Error occured!",
+    });
+    console.log("Error in Update Student ", error);
+  }
+};
 
-      res.status(200).json({
-         message: 'Student Profile Updated',
-         user: student,
-      });
-   } catch (error) {
-      res.status(500).json({
-         message: 'Some Error occured!',
-      });
-      console.log('Error in Update Student ', error);
-   }
+export const getStudentDetails = async (req: Request, res: Response) => {
+  let { username } = req.params;
+
+  const student = await Student.findOne({
+    username: username,
+  });
+
+  console.log(student);
+  if (!student) {
+    return res.status(500).json({
+      message: "Student not found",
+    });
+  }
+
+  res.status(200).json({
+    message: "Student Found Successfully",
+    student,
+  });
 };
--- frontend/src/pages/StudentProfile.tsx
+++ frontend/src/pages/StudentProfile.tsx
@@ -1,8 +1,169 @@
+import { InputLabel } from "@/components/PrimeSkeleton";
+import { StudentType } from "@/components/StudentSearchCard";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { useUserDetails } from "@/utils/store";
+import axios from "axios";
+import { AtSign, CheckCircle, Mail, Trash2 } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const StudentProfilePage = () => {
+  const [loading, setLoading] = useState(false);
+  const [studentDetails, setStudentDetails] = useState<StudentType>();
+  const user = useUserDetails((state) => state.user);
+
+  const { id } = useParams();
+
+  // fetch student details
+  const fetchStudentDetails = async () => {
+    setLoading(true);
+    setLoading(false);
+    try {
+      const data = await axios.get(
+        `http://localhost:3000/api/student/studentdetails/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
+
+      console.log(data);
+      setStudentDetails(data?.data?.student);
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      console.log(err, "Error in student profile page");
+    }
+  };
+
+  // handle student accout deletion
+  const handleDeleteStudentAccount = async () => {};
+
+  useEffect(() => {
+    fetchStudentDetails();
+  }, []);
+
   return (
     <div className="w-full h-fit pl-[1.5rem] sm:pl-[2rem] md:pl-[6rem] pt-[2rem] overflow-y-scroll py-8">
-      <div className="w-full flex justify-between">
-        <h1></h1>
+      <div className="w-full flex justify-between pr-10">
+        <h1 className="text-5xl font-semibold uppercase">
+          {studentDetails?.name}
+        </h1>
+        {user.role == "admin" && (
+          <TooltipProvider>
+            <Tooltip delayDuration={1}>
+              <TooltipTrigger>
+                <button
+                  className="hover:bg-gray-100 rounded-full h-10 w-10 flex justify-center items-center transition-colors duration-300"
+                  onClick={handleDeleteStudentAccount}
+                >
+                  <Trash2 className="size-6" />
+                </button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Delete</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        )}
+      </div>
+      <div className="w-full h-full mt-8">
+        <div className="flex w-full gap-10 flex-wrap">
+          {/* Full Name */}
+          <div className="flex flex-col font-zyada">
+            {!loading ? (
+              <>
+                <label
+                  htmlFor="fullname"
+                  className="font-bold text-2xl flex gap-2"
+                >
+                  FullName
+                </label>
+                <span
+                  id="fullname"
+                  className="bg-[#F7F7F8] rounded-md py-2 text-black text-xl px-2 font-bold tracking-[0.1rem] w-[90%] lg:w-[20rem] outline-none"
+                >
+                  {studentDetails?.name}
+                </span>
+              </>
+            ) : (
+              <InputLabel />
+            )}
+          </div>
+          {/* userName */}
+          <div className="flex flex-col font-zyada">
+            {!loading ? (
+              <>
+                <label
+                  htmlFor="username"
+                  className="font-bold text-2xl items-center flex"
+                >
+                  <AtSign className="size-4" />
+                  username
+                </label>
+                <span
+                  id="username"
+                  className="bg-[#F7F7F8] rounded-md py-2 text-black text-xl px-2 font-bold tracking-[0.1rem] items-center flex w-[90%] lg:w-[20rem] outline-none"
+                >
+                  <AtSign className="size-4" />
+                  {studentDetails?.username}
+                </span>
+              </>
+            ) : (
+              <InputLabel />
+            )}
+          </div>
+          {/* Email */}
+          <div className="flex flex-col font-zyada">
+            {!loading ? (
+              <>
+                <label
+                  htmlFor="email"
+                  className="font-bold text-2xl gap-1 items-center flex"
+                >
+                  Email
+                  <Mail className="size-6 mb-2" />
+                </label>
+                <span
+                  id="email"
+                  className="bg-[#F7F7F8] rounded-md py-2 text-black text-xl px-2 font-bold tracking-[0.1rem] items-center flex w-[90%] lg:w-[20rem] outline-none"
+                >
+                  {studentDetails?.email}
+                </span>
+              </>
+            ) : (
+              <InputLabel />
+            )}
+          </div>
+          {/* Submissions */}
+          <div className="flex flex-col font-zyada">
+            {!loading ? (
+              <>
+                <label
+                  htmlFor="email"
+                  className="font-bold text-2xl gap-1 items-center flex"
+                >
+                  Total Test Submissions
+                  <CheckCircle className="size-4 mb-2" />
+                </label>
+                <span
+                  id="email"
+                  className="bg-[#F7F7F8] rounded-md py-2 text-black text-xl px-2 font-bold tracking-[0.1rem] items-center flex w-[90%] lg:w-[20rem] outline-none"
+                >
+                  {studentDetails?.submissions.length}
+                </span>
+              </>
+            ) : (
+              <InputLabel />
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
