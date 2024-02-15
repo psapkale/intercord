8e63561c9eadf2c74f7002a4c9d6a47acca138d4
Abhay Panchal <abpanchal951357@gmail.com>
2024-02-15 10:33:14 +0530

    chore: connection of backend and frontend socket.io done!

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,26 +1,26 @@
-import express, { Request, Response } from 'express';
-import { router } from './routes';
-import { router as adminRouter } from './routes/admin';
-import { router as teacherRouter } from './routes/teacher';
-import { router as studentRouter } from './routes/student';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express, { Request, Response } from "express";
+import { router } from "./routes";
+import { router as adminRouter } from "./routes/admin";
+import { router as teacherRouter } from "./routes/teacher";
+import { router as studentRouter } from "./routes/student";
+import cors from "cors";
+import dotenv from "dotenv";
+import { server, app } from "./socket/socket";
 
-const app = express();
 const port = 3000;
 
 // Defined routes
 dotenv.config();
 app.use(express.json());
 app.use(cors());
-app.use('/api/admin', adminRouter);
-app.use('/api/teacher', teacherRouter);
-app.use('/api/student', studentRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/teacher", teacherRouter);
+app.use("/api/student", studentRouter);
 
 // common routes
-app.use('/api', router);
+app.use("/api", router);
 
 // Start the server
-app.listen(port, () => {
-   console.log(`Server listening on port: ${3000}`);
+server.listen(port, () => {
+  console.log(`Server listening on port: ${3000}`);
 });
