658293a812aba13a67bd8affa704dde8b93df58a
Abhay Panchal <abpanchal951357@gmail.com>
2024-02-15 16:53:26 +0530

    chore: bookmark added in teacher model

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -37,6 +37,11 @@ const TeacherSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
+  bookmark: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "Test",
+    default: [],
+  },
   createdTests: [
     {
       type: mongoose.Schema.Types.ObjectId,
