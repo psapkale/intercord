344a9edd8c6e6a683b1ade5a742f35f004a88740
Abhay Panchal <abpanchal951357@gmail.com>
2024-02-23 21:45:09 +0530

    chore: Test Icon changed in sidebar

--- frontend/src/components/RankingTable.tsx
+++ frontend/src/components/RankingTable.tsx
@@ -1,47 +1,45 @@
-import ScoreCard from './ScoreCard';
+import ScoreCard from "./ScoreCard";
 
 export type StudentScoreType = {
-   candidate: string;
-   name: string;
-   username: string;
-   submissions: number;
-   score: number;
+  candidate: string;
+  name: string;
+  username: string;
+  submissions: number;
+  score: number;
 };
 
 const RankingTable = ({ allStudents }: { allStudents: StudentScoreType[] }) => {
-   return (
-      <div className='w-full h-full flex flex-col relative overflow-x-hidden'>
-         <div
-            className='bg-white flex w-full py-2 px-2  mb-4 border border-b-black sticky top-0'
-            id='rank'
-         >
-            <h1 className='w-[10%] font-mono font-semibold text-[18px]'>
-               Rank.
-            </h1>
-            <h1 className='w-[60%] truncate font-mono font-semibold text-[18px] ml-[1rem] '>
-               Student Name
-            </h1>
-            <h1 className='w-[20%] hidden md:block font-mono font-semibold text-[18px] truncate'>
-               Submissions
-            </h1>
-            <h1 className='w-[20%] max-md:w-[30%] truncate font-mono font-semibold text-[18px]'>
-               Total Score
-            </h1>
-         </div>
-         {allStudents.map((student, idx) => {
-            return (
-               <ScoreCard
-                  name={student.name}
-                  username={student.username}
-                  rank={idx + 1 + ''}
-                  score={student.score}
-                  submission={student.submissions}
-                  key={idx}
-               />
-            );
-         })}{' '}
+  return (
+    <div className="w-full h-full flex flex-col relative overflow-x-hidden">
+      <div
+        className="bg-white flex w-full py-2 px-2  mb-4 border border-b-black sticky top-0"
+        id="rank"
+      >
+        <h1 className="w-[10%]  font-semibold text-[18px]">Rank.</h1>
+        <h1 className="w-[60%] truncate  font-semibold text-[18px] ml-[1rem] ">
+          Student Name
+        </h1>
+        <h1 className="w-[20%] hidden md:block  font-semibold text-[18px] truncate">
+          Submissions
+        </h1>
+        <h1 className="w-[20%] max-md:w-[30%] truncate  font-semibold text-[18px]">
+          Total Score
+        </h1>
       </div>
-   );
+      {allStudents.map((student, idx) => {
+        return (
+          <ScoreCard
+            name={student.name}
+            username={student.username}
+            rank={idx + 1 + ""}
+            score={student.score}
+            submission={student.submissions}
+            key={idx}
+          />
+        );
+      })}{" "}
+    </div>
+  );
 };
 
 export default RankingTable;
--- frontend/src/pages/LeaderBoard.tsx
+++ frontend/src/pages/LeaderBoard.tsx
@@ -1,145 +1,143 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react-hooks/rules-of-hooks */
-import RankingTable from '@/components/RankingTable';
-import TestTable from '@/components/TestTable';
-import useFetchingDataFn from '@/hook/useFetchingDataFn';
-import { callLeaderBoardDriver } from '@/utils/driver';
-import { useUserDetails } from '@/utils/store';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { TestType } from './GiveTest';
+import RankingTable from "@/components/RankingTable";
+import TestTable from "@/components/TestTable";
+import useFetchingDataFn from "@/hook/useFetchingDataFn";
+import { callLeaderBoardDriver } from "@/utils/driver";
+import { useUserDetails } from "@/utils/store";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { TestType } from "./GiveTest";
 
 const LeaderBoard = () => {
-   const { user, updateTutorial } = useUserDetails((state) => ({
-      user: state.user,
-      updateTutorial: state.updateTutorial,
-   }));
+  const { user, updateTutorial } = useUserDetails((state) => ({
+    user: state.user,
+    updateTutorial: state.updateTutorial,
+  }));
 
-   // destructuring the data from the hook
-   const {
-      selectedOptionField,
-      setIsRankingOpen,
-      setSelectedOptionField,
-      isRankingOpen,
-   } = useFetchingDataFn();
+  // destructuring the data from the hook
+  const {
+    selectedOptionField,
+    setIsRankingOpen,
+    setSelectedOptionField,
+    isRankingOpen,
+  } = useFetchingDataFn();
 
-   const [allStudents, setAllStudents] = useState([]);
-   const [allTests, setAllTests] = useState<TestType[]>([]);
+  const [allStudents, setAllStudents] = useState([]);
+  const [allTests, setAllTests] = useState<TestType[]>([]);
 
-   //<-- checking if we need to display the tutorial or not -->
-   if (user.isSignedUp && user.leaderboardDriverJs) {
-      updateTutorial('leaderboardDriverJs');
-      callLeaderBoardDriver();
-   }
+  //<-- checking if we need to display the tutorial or not -->
+  if (user.isSignedUp && user.leaderboardDriverJs) {
+    updateTutorial("leaderboardDriverJs");
+    callLeaderBoardDriver();
+  }
 
-   useEffect(() => {
-      if (selectedOptionField !== 'all') getAllTestBySelectedSubject();
-      else {
-         getAllStudentRanking();
-      }
-   }, [selectedOptionField]);
+  useEffect(() => {
+    if (selectedOptionField !== "all") getAllTestBySelectedSubject();
+    else {
+      getAllStudentRanking();
+    }
+  }, [selectedOptionField]);
 
-   const getAllStudentRanking = async () => {
-      const res = await axios.get('http://localhost:3000/api/score-board/all');
+  const getAllStudentRanking = async () => {
+    const res = await axios.get("http://localhost:3000/api/score-board/all");
 
-      res?.data?.scoreBoard && setAllStudents(res?.data?.scoreBoard);
-      return;
-   };
+    res?.data?.scoreBoard && setAllStudents(res?.data?.scoreBoard);
+    return;
+  };
 
-   const getAllStudentsBySelectedSubject = async () => {
-      const res = await axios.get(
-         `http://localhost:3000/api/score-board/subject/${selectedOptionField}`
-      );
+  const getAllStudentsBySelectedSubject = async () => {
+    const res = await axios.get(
+      `http://localhost:3000/api/score-board/subject/${selectedOptionField}`
+    );
 
-      res?.data?.formattedRes && setAllStudents(res?.data?.formattedRes);
-      return;
-   };
+    res?.data?.formattedRes && setAllStudents(res?.data?.formattedRes);
+    return;
+  };
 
-   const getAllTestBySelectedSubject = async () => {
-      const res = await axios.get(
-         `http://localhost:3000/api/score-board/test/${selectedOptionField}`
-      );
+  const getAllTestBySelectedSubject = async () => {
+    const res = await axios.get(
+      `http://localhost:3000/api/score-board/test/${selectedOptionField}`
+    );
 
-      res?.data?.tests && setAllTests(res?.data?.tests);
-      return;
-   };
+    res?.data?.tests && setAllTests(res?.data?.tests);
+    return;
+  };
 
-   return (
-      <div className='w-full h-[100vh] max-sm:px-[1rem] sm:pl-[1.5rem] md:pl-[3rem] lg:pl-[6rem] pt-[2rem] overflow-y-scroll overflow-x-hidden py-8'>
-         <div className='w-full md:w-[95%] h-[95%]'>
-            <div className='flex flex-col'>
-               <div>
-                  <h1
-                     className='font-mono font-semibold text-[3rem] sm:text-[3.5rem] -mb-4'
-                     id='title'
-                  >
-                     Leaderboard
-                  </h1>
-                  <p className='pl-1'>
-                     Here you can see your and others's ranking
-                  </p>
-               </div>
-               <div className='px-[1rem] pl-1 w-full flex justify-between items-center mt-4'>
-                  <div className='flex gap-2 sm:gap-4'>
-                     <button
-                        className={`px-2 py-1 rounded-md bg-gray-100 ${
-                           selectedOptionField === 'all' && 'cursor-not-allowed'
-                        } ${
-                           !isRankingOpen &&
-                           selectedOptionField !== 'all' &&
-                           'bg-gray-300'
-                        }`}
-                        disabled={selectedOptionField === 'all'}
-                        onClick={() => {
-                           setIsRankingOpen(false);
-                        }}
-                     >
-                        Tests
-                     </button>
-                     <button
-                        className={`px-2 py-1 rounded-md bg-gray-100 ${
-                           selectedOptionField === 'all' && 'cursor-not-allowed'
-                        } ${
-                           isRankingOpen &&
-                           selectedOptionField !== 'all' &&
-                           'bg-gray-300'
-                        }`}
-                        disabled={selectedOptionField === 'all'}
-                        onClick={() => {
-                           setIsRankingOpen(true);
-                           getAllStudentsBySelectedSubject();
-                        }}
-                     >
-                        Ranking
-                     </button>
-                  </div>
-                  <select
-                     className='outline-none bg-gray-100 p-2 rounded-md'
-                     onChange={(e) => {
-                        setSelectedOptionField(e.target.value);
-                        if (e.target.value == 'all') setIsRankingOpen(false);
-                     }}
-                  >
-                     <option value='all'>All</option>
-                     <option value='php'>Php</option>
-                     <option value='java'>Java</option>
-                     <option value='c++'>c++</option>
-                  </select>
-               </div>
-            </div>
-            <div
-               className='w-full h-[90%] border border-black overflow-scroll rounded-md shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] mt-4'
-               id='rankingTable'
+  return (
+    <div className="w-full h-[100vh] max-sm:px-[1rem] sm:pl-[1.5rem] md:pl-[3rem] lg:pl-[6rem] pt-[2rem] overflow-y-scroll overflow-x-hidden py-8">
+      <div className="w-full md:w-[95%] h-[95%]">
+        <div className="flex flex-col">
+          <div>
+            <h1
+              className="font-mono font-semibold text-[3rem] sm:text-[3.5rem] -mb-4"
+              id="title"
             >
-               {selectedOptionField == 'all' || isRankingOpen ? (
-                  <RankingTable allStudents={allStudents} />
-               ) : (
-                  <TestTable allTests={allTests} />
-               )}
+              Leaderboard
+            </h1>
+            <p className="pl-1">Here you can see your and others's ranking</p>
+          </div>
+          <div className="px-[1rem] pl-1 w-full flex justify-between items-center mt-4">
+            <div className="flex gap-2 sm:gap-4">
+              <button
+                className={`px-2 py-1 rounded-md bg-gray-100 ${
+                  selectedOptionField === "all" && "cursor-not-allowed"
+                } ${
+                  !isRankingOpen &&
+                  selectedOptionField !== "all" &&
+                  "bg-gray-300"
+                }`}
+                disabled={selectedOptionField === "all"}
+                onClick={() => {
+                  setIsRankingOpen(false);
+                }}
+              >
+                Tests
+              </button>
+              <button
+                className={`px-2 py-1 rounded-md bg-gray-100 ${
+                  selectedOptionField === "all" && "cursor-not-allowed"
+                } ${
+                  isRankingOpen &&
+                  selectedOptionField !== "all" &&
+                  "bg-gray-300"
+                }`}
+                disabled={selectedOptionField === "all"}
+                onClick={() => {
+                  setIsRankingOpen(true);
+                  getAllStudentsBySelectedSubject();
+                }}
+              >
+                Ranking
+              </button>
             </div>
-         </div>
+            <select
+              className="outline-none bg-gray-100 p-2 rounded-md"
+              onChange={(e) => {
+                setSelectedOptionField(e.target.value);
+                if (e.target.value == "all") setIsRankingOpen(false);
+              }}
+            >
+              <option value="all">All</option>
+              <option value="php">Php</option>
+              <option value="java">Java</option>
+              <option value="c++">c++</option>
+            </select>
+          </div>
+        </div>
+        <div
+          className="w-full h-[90%] border border-black overflow-scroll rounded-md shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] mt-4"
+          id="rankingTable"
+        >
+          {selectedOptionField == "all" || isRankingOpen ? (
+            <RankingTable allStudents={allStudents} />
+          ) : (
+            <TestTable allTests={allTests} />
+          )}
+        </div>
       </div>
-   );
+    </div>
+  );
 };
 
 export default LeaderBoard;
