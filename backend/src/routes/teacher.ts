cbae9d4f99cc6e0b9ad8b1f335bcbf90c58afdcd
Prem <prempravinsapkale@gmail.com>
2024-03-02 21:18:40 +0530

    teacher stream and pursuing year resolved

--- backend/src/routes/teacher.ts
+++ backend/src/routes/teacher.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express';
 import {
+   allStudents,
+   allTeachers,
    allowStudentRegister,
    createTest,
    getAllPendingStudents,
+   getClosedTests,
+   getLiveTests,
+   getScoreBoard,
+   getSubjectFilteredScoreBoard,
+   getSubjectFilteredTests,
+   getUpComingTests,
    rejectStudentRegister,
+   serachStudent,
+   serachTeacher,
    teacherLogin,
    updateSeenTeacher,
 } from '../controllers/teacher';
@@ -19,5 +29,24 @@ router.get('/allow/:username', teacherMiddleware, allowStudentRegister);
 router.get('/reject/:username', teacherMiddleware, rejectStudentRegister);
 router.get('/allrequest', teacherMiddleware, getAllPendingStudents);
 router.put('/updateseen', teacherMiddleware, updateSeenTeacher);
+
+router.get('/search/student/all', teacherMiddleware, allStudents);
+router.get('/search/teacher/all', teacherMiddleware, allTeachers);
+router.get('/search/student/:username', teacherMiddleware, serachStudent);
+router.get('/search/teacher/:username', teacherMiddleware, serachTeacher);
+router.get('/score-board/all', teacherMiddleware, getScoreBoard);
+router.get(
+   '/score-board/subject/:subject',
+   teacherMiddleware,
+   getSubjectFilteredScoreBoard
+);
+router.get(
+   '/score-board/test/:subject',
+   teacherMiddleware,
+   getSubjectFilteredTests
+);
+router.get('/upcoming', teacherMiddleware, getUpComingTests);
+router.get('/closed', teacherMiddleware, getClosedTests);
+router.get('/live', teacherMiddleware, getLiveTests);
 
 export { router };
