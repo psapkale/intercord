6ee50e03986880467fcb085e0637cd9ad150f36b
Abhay Panchal <abpanchal951357@gmail.com>
2024-03-02 20:12:52 +0530

    chore: conflict resolved

--- backend/src/controllers/student.ts
+++ backend/src/controllers/student.ts
@@ -1,8 +1,14 @@
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import { Request, Response } from 'express';
-import { PendingStudent, Score, Student, Teacher, Test } from '../db';
-import { StudentType, SubjectScoreType, TeacherType, TestType } from '../types';
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { Request, Response } from "express";
+import { PendingStudent, Score, Student, Teacher, Test } from "../db";
+import {
+  ScoreType,
+  StudentType,
+  SubjectScoreType,
+  TeacherType,
+  TestType,
+} from "../types";
 
 // Todo all mongo logic here
 // Todo restrict creating duplicate users
@@ -138,15 +144,15 @@ export const getTestById = async (req: Request, res: Response) => {
     });
   }
 
-   let test: TestType | null = await Test.findOne({ _id: testId });
+  let test: TestType | null = await Test.findOne({ _id: testId });
 
-   if (!test) {
-      return res.status(404).json({
-         message: 'Test not found',
-      });
-   }
+  if (!test) {
+    return res.status(404).json({
+      message: "Test not found",
+    });
+  }
 
-   const student: StudentType | null = await Student.findOne({ username });
+  const student: StudentType | null = await Student.findOne({ username });
 
   const isRepeat = test?.submissions?.find(
     (submission) => String(student?._id) == String(submission?.submittedBy)
@@ -212,7 +218,7 @@ export const testSubmission = async (req: Request, res: Response) => {
 
   let student: StudentType | null = await Student.findOne({ username });
 
-   const test: TestType | null = await Test.findOne({ _id: testId });
+  const test: TestType | null = await Test.findOne({ _id: testId });
 
   const marksPerQuestion = test?.totalMarks / test?.questions?.length;
 
@@ -232,21 +238,21 @@ export const testSubmission = async (req: Request, res: Response) => {
 
   await test?.save();
 
-   student = await Student.findOneAndUpdate(
-      { username },
-      {
-         $push: {
-            submissions: {
-               test: test._id,
-               subject: test.subject,
-               submittedAnswersIndex: submittedAnswersIndex,
-               marksObtained: marksObtained,
-               submittedAt: new Date(Date.now()),
-            },
-         },
+  student = await Student.findOneAndUpdate(
+    { username },
+    {
+      $push: {
+        submissions: {
+          test: test._id,
+          subject: test.subject,
+          submittedAnswersIndex: submittedAnswersIndex,
+          marksObtained: marksObtained,
+          submittedAt: new Date(Date.now()),
+        },
       },
-      { new: true }
-   );
+    },
+    { new: true }
+  );
 
   if (!student) {
     return res.status(500).json({
@@ -275,17 +281,17 @@ export const testSubmission = async (req: Request, res: Response) => {
     console.error("student.subjectScore is not an array");
   }
 
-   // Todo resove score data
-   const score = await Score.findOneAndUpdate(
-      { candidate: student._id },
-      {
-         $inc: {
-            score: marksObtained,
-         },
-         submissions: student?.submissions?.length,
+  // Todo resove score data
+  const score = await Score.findOneAndUpdate(
+    { candidate: student._id },
+    {
+      $inc: {
+        score: marksObtained,
       },
-      { new: true }
-   );
+      submissions: student?.submissions?.length,
+    },
+    { new: true }
+  );
 
   res.json({
     message: "Test submitted successfully",
@@ -420,310 +426,310 @@ export const updateSeenStudent = async (req: Request, res: Response) => {
 };
 
 export const allStudents = async (req: Request, res: Response) => {
-   const { username } = res.locals;
+  const { username } = res.locals;
 
-   const student: StudentType | null = await Student.findOne({ username });
+  const student: StudentType | null = await Student.findOne({ username });
 
-   const allStudents: StudentType[] | null = await Student.find({
-      stream: student.stream,
-      pursuingYear: student.pursuingYear,
-   });
+  const allStudents: StudentType[] | null = await Student.find({
+    stream: student.stream,
+    pursuingYear: student.pursuingYear,
+  });
 
-   if (!allStudents) {
-      return res.status(500).json({
-         message: 'Failed to get all students',
-      });
-   }
+  if (!allStudents) {
+    return res.status(500).json({
+      message: "Failed to get all students",
+    });
+  }
 
-   res.status(200).json({ message: 'Done Successfully', allStudents });
+  res.status(200).json({ message: "Done Successfully", allStudents });
 };
 
 export const allTeachers = async (req: Request, res: Response) => {
-   const { username } = res.locals;
+  const { username } = res.locals;
 
-   const student: StudentType | null = await Student.findOne({ username });
+  const student: StudentType | null = await Student.findOne({ username });
 
-   const allTeachers: TeacherType[] | null = await Teacher.find({
-      stream: student.stream,
-   });
+  const allTeachers: TeacherType[] | null = await Teacher.find({
+    stream: student.stream,
+  });
 
-   if (!allTeachers) {
-      return res.status(500).json({
-         message: 'Failed to get all teachers',
-      });
-   }
+  if (!allTeachers) {
+    return res.status(500).json({
+      message: "Failed to get all teachers",
+    });
+  }
 
-   res.status(200).json({ message: 'Done Successfully', allTeachers });
+  res.status(200).json({ message: "Done Successfully", allTeachers });
 };
 
 export const serachStudent = async (req: Request, res: Response) => {
-   const { username } = req.params;
-   const currentStudentUsername = res.locals.username;
+  const { username } = req.params;
+  const currentStudentUsername = res.locals.username;
 
-   if (!username) {
-      return res.status(400).json({
-         message: 'Please provide student name',
-      });
-   }
+  if (!username) {
+    return res.status(400).json({
+      message: "Please provide student name",
+    });
+  }
 
-   const currentStudent: StudentType | null = await Student.findOne({
-      username: currentStudentUsername,
-   });
+  const currentStudent: StudentType | null = await Student.findOne({
+    username: currentStudentUsername,
+  });
 
-   const resStudent = await Student.findOne({
-      username,
-      stream: currentStudent.stream,
-      pursuingYear: currentStudent.pursuingYear,
-   });
+  const resStudent = await Student.findOne({
+    username,
+    stream: currentStudent.stream,
+    pursuingYear: currentStudent.pursuingYear,
+  });
 
-   if (!resStudent) {
-      return res.status(404).json({
-         message: 'Student not found in the database',
-      });
-   }
+  if (!resStudent) {
+    return res.status(404).json({
+      message: "Student not found in the database",
+    });
+  }
 
-   res.status(200).json({
-      resStudent,
-   });
+  res.status(200).json({
+    resStudent,
+  });
 };
 
 export const serachTeacher = async (req: Request, res: Response) => {
-   const { username } = req.params;
-   const currentStudentUsername = res.locals.username;
+  const { username } = req.params;
+  const currentStudentUsername = res.locals.username;
 
-   if (!username) {
-      return res.status(400).json({
-         message: 'Please provide student name',
-      });
-   }
+  if (!username) {
+    return res.status(400).json({
+      message: "Please provide student name",
+    });
+  }
 
-   const currentStudent: StudentType | null = await Student.findOne({
-      username: currentStudentUsername,
-   });
+  const currentStudent: StudentType | null = await Student.findOne({
+    username: currentStudentUsername,
+  });
 
-   const teacher: TeacherType | null = await Teacher.findOne({
-      username,
-      stream: currentStudent.stream,
-   });
+  const teacher: TeacherType | null = await Teacher.findOne({
+    username,
+    stream: currentStudent.stream,
+  });
 
-   if (!teacher) {
-      return res.status(404).json({
-         message: 'Teacher not found in the database',
-      });
-   }
+  if (!teacher) {
+    return res.status(404).json({
+      message: "Teacher not found in the database",
+    });
+  }
 
-   res.status(200).json({
-      teacher,
-   });
+  res.status(200).json({
+    teacher,
+  });
 };
 
 export const getScoreBoard = async (req: Request, res: Response) => {
-   const { username } = res.locals;
+  const { username } = res.locals;
 
-   const student: StudentType | null = await Student.findOne({ username });
+  const student: StudentType | null = await Student.findOne({ username });
 
-   // Todo check null points
-   const scoreBoard: ScoreType[] | null = await Score.find({
-      stream: student.stream,
-      pursuingYear: student.pursuingYear,
-   }).sort({
-      score: -1,
-   });
+  // Todo check null points
+  const scoreBoard: ScoreType[] | null = await Score.find({
+    stream: student.stream,
+    pursuingYear: student.pursuingYear,
+  }).sort({
+    score: -1,
+  });
 
-   if (!scoreBoard) {
-      return res.status(500).json({
-         message: "Opps! can't load leaderboard",
-      });
-   }
+  if (!scoreBoard) {
+    return res.status(500).json({
+      message: "Opps! can't load leaderboard",
+    });
+  }
 
-   res.status(200).json({
-      message: 'Done Successfully',
-      scoreBoard,
-   });
+  res.status(200).json({
+    message: "Done Successfully",
+    scoreBoard,
+  });
 };
 
 export const getSubjectFilteredScoreBoard = async (
-   req: Request,
-   res: Response
+  req: Request,
+  res: Response
 ) => {
-   const { subject } = req.params;
-   const { username } = res.locals;
+  const { subject } = req.params;
+  const { username } = res.locals;
 
-   if (!subject) {
-      return res.status(400).json({
-         message: 'Please provide subject name',
-      });
-   }
+  if (!subject) {
+    return res.status(400).json({
+      message: "Please provide subject name",
+    });
+  }
 
-   const student: StudentType | null = await Student.findOne({ username });
+  const student: StudentType | null = await Student.findOne({ username });
 
-   const students: StudentType[] | null = await Student.find({
-      'subjectScore.subject': subject,
-      stream: student.stream,
-      pursuingYear: student.pursuingYear,
-   });
+  const students: StudentType[] | null = await Student.find({
+    "subjectScore.subject": subject,
+    stream: student.stream,
+    pursuingYear: student.pursuingYear,
+  });
 
-   if (!students) {
-      return res.status(500).json({
-         message: 'Failed to retrieve students',
-      });
-   }
+  if (!students) {
+    return res.status(500).json({
+      message: "Failed to retrieve students",
+    });
+  }
 
-   // Todo remaining with testing
-   const sortedStudents = students.sort((x, y) => {
-      const xSubjectScore = x.subjectScore.find((z) => z.subject === subject);
-      const ySubjectScore = y.subjectScore.find((z) => z.subject === subject);
+  // Todo remaining with testing
+  const sortedStudents = students.sort((x, y) => {
+    const xSubjectScore = x.subjectScore.find((z) => z.subject === subject);
+    const ySubjectScore = y.subjectScore.find((z) => z.subject === subject);
 
-      if (xSubjectScore && ySubjectScore) {
-         return ySubjectScore.score - xSubjectScore.score;
-      } else {
-         return 0;
-      }
-   });
+    if (xSubjectScore && ySubjectScore) {
+      return ySubjectScore.score - xSubjectScore.score;
+    } else {
+      return 0;
+    }
+  });
 
-   const formattedRes = sortedStudents.map((student) => {
-      const requiredSubject: SubjectScoreType = student?.subjectScore?.find(
-         (x) => x?.subject === subject
-      );
+  const formattedRes = sortedStudents.map((student) => {
+    const requiredSubject: SubjectScoreType = student?.subjectScore?.find(
+      (x) => x?.subject === subject
+    );
 
-      const filteredSubmission = student.submissions.filter((submission) => {
-         return submission.subject === subject;
-      });
+    const filteredSubmission = student.submissions.filter((submission) => {
+      return submission.subject === subject;
+    });
 
-      return {
-         name: student?.name,
-         username: student?.username,
-         submissions: filteredSubmission?.length,
-         score: requiredSubject?.score,
-      };
-   });
+    return {
+      name: student?.name,
+      username: student?.username,
+      submissions: filteredSubmission?.length,
+      score: requiredSubject?.score,
+    };
+  });
 
-   res.status(200).json({
-      message: 'Done Successfully',
-      formattedRes,
-   });
+  res.status(200).json({
+    message: "Done Successfully",
+    formattedRes,
+  });
 };
 
 export const getSubjectFilteredTests = async (req: Request, res: Response) => {
-   const { subject } = req.params;
-   const { username } = res.locals;
+  const { subject } = req.params;
+  const { username } = res.locals;
 
-   if (!subject) {
-      return res.status(400).json({
-         message: 'Please provide subject name',
-      });
-   }
+  if (!subject) {
+    return res.status(400).json({
+      message: "Please provide subject name",
+    });
+  }
 
-   const student: StudentType | null = await Student.findOne({ username });
+  const student: StudentType | null = await Student.findOne({ username });
 
-   // replacing any special characters in the 'subject'
-   const escapedSubject = subject.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-   const tests: TestType[] | null = await Test.find({
-      subject: { $regex: new RegExp(escapedSubject, 'i') },
-      stream: student.stream,
-      forYear: student.pursuingYear,
-   });
+  // replacing any special characters in the 'subject'
+  const escapedSubject = subject.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  const tests: TestType[] | null = await Test.find({
+    subject: { $regex: new RegExp(escapedSubject, "i") },
+    stream: student.stream,
+    forYear: student.pursuingYear,
+  });
 
-   if (!tests) {
-      return res.status(500).json({
-         message: 'Failed to retrieve tests',
-      });
-   }
+  if (!tests) {
+    return res.status(500).json({
+      message: "Failed to retrieve tests",
+    });
+  }
 
-   res.status(200).json({
-      message: 'Done Successfully',
-      tests,
-   });
+  res.status(200).json({
+    message: "Done Successfully",
+    tests,
+  });
 };
 
 export const getUpComingTests = async (req: Request, res: Response) => {
-   const { username } = res.locals;
+  const { username } = res.locals;
 
-   const student: StudentType | null = await Student.findOne({ username });
+  const student: StudentType | null = await Student.findOne({ username });
 
-   const currentDateTime = new Date();
-   const options = { timeZone: 'Asia/Kolkata', hour12: false };
-   const indianTime = currentDateTime.toLocaleString('en-US', options);
+  const currentDateTime = new Date();
+  const options = { timeZone: "Asia/Kolkata", hour12: false };
+  const indianTime = currentDateTime.toLocaleString("en-US", options);
 
-   const upcoming = await Test.find({
-      $and: [
-         { stream: student.stream, forYear: student.pursuingYear },
-         {
-            $or: [
-               {
-                  startDate: {
-                     $gt: currentDateTime.toISOString().slice(0, 10),
-                  },
-               },
-               {
-                  startDate: currentDateTime.toISOString().slice(0, 10),
-                  time: { $gt: indianTime.slice(11, 16) },
-               },
-            ],
-         },
-      ],
-   });
+  const upcoming = await Test.find({
+    $and: [
+      { stream: student.stream, forYear: student.pursuingYear },
+      {
+        $or: [
+          {
+            startDate: {
+              $gt: currentDateTime.toISOString().slice(0, 10),
+            },
+          },
+          {
+            startDate: currentDateTime.toISOString().slice(0, 10),
+            time: { $gt: indianTime.slice(11, 16) },
+          },
+        ],
+      },
+    ],
+  });
 
-   res.status(200).json({
-      message: 'Done Successfully',
-      upcoming,
-   });
+  res.status(200).json({
+    message: "Done Successfully",
+    upcoming,
+  });
 };
 
 export const getClosedTests = async (req: Request, res: Response) => {
-   const { username } = res.locals;
+  const { username } = res.locals;
 
-   const student: StudentType | null = await Student.findOne({ username });
+  const student: StudentType | null = await Student.findOne({ username });
 
-   const currentDateTime = new Date();
-   const options = { timeZone: 'Asia/Kolkata', hour12: false };
-   const indianTime = currentDateTime.toLocaleString('en-US', options);
+  const currentDateTime = new Date();
+  const options = { timeZone: "Asia/Kolkata", hour12: false };
+  const indianTime = currentDateTime.toLocaleString("en-US", options);
 
-   const closed = await Test.find({
-      $and: [
-         { stream: student.stream, forYear: student.pursuingYear },
-         {
-            $or: [
-               {
-                  startDate: {
-                     $lt: currentDateTime.toISOString().slice(0, 10),
-                  },
-               },
-               {
-                  startDate: currentDateTime.toISOString().slice(0, 10),
-                  endTime: { $lt: indianTime.slice(11, 16) },
-               },
-            ],
-         },
-      ],
-   });
+  const closed = await Test.find({
+    $and: [
+      { stream: student.stream, forYear: student.pursuingYear },
+      {
+        $or: [
+          {
+            startDate: {
+              $lt: currentDateTime.toISOString().slice(0, 10),
+            },
+          },
+          {
+            startDate: currentDateTime.toISOString().slice(0, 10),
+            endTime: { $lt: indianTime.slice(11, 16) },
+          },
+        ],
+      },
+    ],
+  });
 
-   res.status(200).json({
-      message: 'Done Successfully',
-      closed,
-   });
+  res.status(200).json({
+    message: "Done Successfully",
+    closed,
+  });
 };
 
 export const getLiveTests = async (req: Request, res: Response) => {
-   const { username } = res.locals;
+  const { username } = res.locals;
 
-   const student: StudentType | null = await Student.findOne({ username });
+  const student: StudentType | null = await Student.findOne({ username });
 
-   const currentDateTime = new Date();
-   const options = { timeZone: 'Asia/Kolkata', hour12: false };
-   const indianTime = currentDateTime.toLocaleString('en-US', options);
+  const currentDateTime = new Date();
+  const options = { timeZone: "Asia/Kolkata", hour12: false };
+  const indianTime = currentDateTime.toLocaleString("en-US", options);
 
-   const live = await Test.find({
-      $and: [
-         { stream: student.stream, forYear: student.pursuingYear },
-         { startDate: currentDateTime.toISOString().slice(0, 10) },
-         { time: { $lte: indianTime.slice(11, 16) } },
-         { endTime: { $gte: indianTime.slice(11, 16) } },
-      ],
-   });
+  const live = await Test.find({
+    $and: [
+      { stream: student.stream, forYear: student.pursuingYear },
+      { startDate: currentDateTime.toISOString().slice(0, 10) },
+      { time: { $lte: indianTime.slice(11, 16) } },
+      { endTime: { $gte: indianTime.slice(11, 16) } },
+    ],
+  });
 
-   res.status(200).json({
-      message: 'Successfully Fetched the live test',
-      live,
-   });
+  res.status(200).json({
+    message: "Successfully Fetched the live test",
+    live,
+  });
 };
