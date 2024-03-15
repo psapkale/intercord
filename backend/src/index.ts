feaa793c738fba1d5ac79ed2a64e6f8112aea36d
Prem <prempravinsapkale@gmail.com>
2024-03-15 20:03:06 +0530

    added root path

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,11 @@
-import express, { Request, Response } from "express";
-import { router } from "./routes";
-import { router as adminRouter } from "./routes/admin";
-import { router as teacherRouter } from "./routes/teacher";
-import { router as studentRouter } from "./routes/student";
-import cors from "cors";
-import dotenv from "dotenv";
-import { server, app } from "./socket/socket";
+import express, { Request, Response } from 'express';
+import { router } from './routes';
+import { router as adminRouter } from './routes/admin';
+import { router as teacherRouter } from './routes/teacher';
+import { router as studentRouter } from './routes/student';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { server, app } from './socket/socket';
 
 const port = 3000;
 
@@ -13,14 +13,18 @@ const port = 3000;
 dotenv.config();
 app.use(express.json());
 app.use(cors());
-app.use("/api/admin", adminRouter);
-app.use("/api/teacher", teacherRouter);
-app.use("/api/student", studentRouter);
+app.use('/api/admin', adminRouter);
+app.use('/api/teacher', teacherRouter);
+app.use('/api/student', studentRouter);
 
 // common routes
-app.use("/api", router);
+app.use('/api', router);
+
+app.get('/', (req, res) => {
+   res.send('Server is live');
+});
 
 // Start the server
 server.listen(port, () => {
-  console.log(`Server listening on port: ${3000}`);
+   console.log(`Server listening on port: ${3000}`);
 });
